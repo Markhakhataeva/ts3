bfd72ae3d8e07cba0b1e2fd638925adf466fcd08
mike <mike@hk>
2024-04-16 17:56:17 +0300

    first com

---
+++ index.js
@@ -0,0 +1,89 @@
+// enum DaysOfWeek{
+//     monday,
+//     tuesday,
+//     wednesday,
+//     thursday,
+//     friday,
+//     saturday,
+//     sunduy
+// }
+// console.log(DaysOfWeek[0])
+// console.log(DaysOfWeek[1])
+// console.log(DaysOfWeek[2])
+// console.log(DaysOfWeek[3])
+// console.log(DaysOfWeek[4])
+// console.log(DaysOfWeek[5])
+// console.log(DaysOfWeek[6])
+// console.log(DaysOfWeek[7])
+////////////
+// enum Months{
+//     december,
+//     january,
+//     february,
+//     march,
+//     april,
+//     may,
+//     june,
+//     july,
+//     august,
+//     september,
+//     october,
+//     november
+// }
+// function man(n){
+//     if(0<n ||n<12){
+//         console.log("введите от 1 до 12")
+//     }else{
+//         console.log(Months[n])
+//     }
+// }
+// console.log(Months[11])
+////////
+// enum Colors{
+//     blue,
+//     red,
+//     purple,
+//     yellow,
+//     orange
+// }
+// function shade(n){
+//    if(0<n||n<5){
+//     console.log(Colors[n])
+//    }
+// }
+// console.log(Colors[2])
+//////////////
+// enum Planets{
+//     mercury=1,
+//     venus=2,
+//     earth=3,
+//     mars=4,
+//     jupiter=5,
+//     saturn=6,
+//     uranus=7,
+//     neptune=8
+// }
+// function planet(name){
+//     if(name){console.log(Planets)}
+// }
+// console.log(Planets.earth)
+//////////
+var Fruits;
+(function (Fruits) {
+    Fruits[Fruits["apple"] = 0] = "apple";
+    Fruits[Fruits["orange"] = 1] = "orange";
+    Fruits[Fruits["banana"] = 2] = "banana";
+    Fruits[Fruits["avocado"] = 3] = "avocado";
+    Fruits[Fruits["lime"] = 4] = "lime";
+})(Fruits || (Fruits = {}));
+function fruit(element) {
+    if (Fruits[element]) {
+        console.log("cool");
+    }
+    else {
+        console.log("i dont know this fruit");
+    }
+
+}
+
+console.log(Fruits[2]);
